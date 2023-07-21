b7fb3c7543b23858b1addfca6f5d2de7c1c5fe52
CrimsonCoder42 <nostro37@gmail.com>
2023-07-21 07:35:22 -0400

    start beautiful soup

---
+++ main.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import lxml
+
+with open('website.html') as file:
+    contents = file.read()
+
+
+soup = BeautifulSoup(contents, 'html.parser')
+print(soup.title)
+print(soup.title.string)
+
+print(soup.p)
